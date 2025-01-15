e148ad671404b912f2db3a2d623b8ed46caca3dc
24kcsplus <1969262592@qq.com>
2025-01-15 14:43:21 +0800

    level 2-1 Get 请求 + 解析 json

---
+++ app/src/main/java/com/example/netrequest/JsonDataListener.java
@@ -0,0 +1,5 @@
+package com.example.netrequest;
+
+public interface JsonDataListener {
+    void onDataReceived(JsonData jsonData);
+}
--- app/src/main/java/com/example/netrequest/MainActivity.java
+++ app/src/main/java/com/example/netrequest/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.netrequest;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -13,99 +16,106 @@ import androidx.core.view.WindowInsetsCompat;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTvJson;
+    private Handler mHandler;
+
+    private JsonData jsonData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
-
-        // JSON 字符串
-        String jsonString = "{\n" +
-                "\"data\":[\n" +
-                "{\n" +
-                "\"id\":6,\n" +
-                "\"link\":\"\",\n" +
-                "\"name\":\"面试\",\n" +
-                "\"order\":1,\n" +
-                "\"visible\":1\n" +
-                "},\n" +
-                "{\n" +
-                "\"id\":9,\n" +
-                "\"link\":\"\",\n" +
-                "\"name\":\"Studio3\",\n" +
-                "\"order\":1,\n" +
-                "\"visible\":1\n" +
-                "},\n" +
-                "{\n" +
-                "\"id\":5,\n" +
-                "\"link\":\"\",\n" +
-                "\"name\":\"动画\",\n" +
-                "\"order\":2,\n" +
-                "\"visible\":1\n" +
-                "},\n" +
-                "{\n" +
-                "\"id\":1,\n" +
-                "\"link\":\"\",\n" +
-                "\"name\":\"自定义View\",\n" +
-                "\"order\":3,\n" +
-                "\"visible\":1\n" +
-                "},\n" +
-                "{\n" +
-                "\"id\":2,\n" +
-                "\"link\":\"\",\n" +
-                "\"name\":\"性能优化 速度\",\n" +
-                "\"order\":4,\n" +
-                "\"visible\":1\n" +
-                "},\n" +
-                "{\n" +
-                "\"id\":3,\n" +
-                "\"link\":\"\",\n" +
-                "\"name\":\"gradle\",\n" +
-                "\"order\":5,\n" +
-                "\"visible\":1\n" +
-                "},\n" +
-                "{\n" +
-                "\"id\":4,\n" +
-                "\"link\":\"\",\n" +
-                "\"name\":\"Camera 相机\",\n" +
-                "\"order\":6,\n" +
-                "\"visible\":1\n" +
-                "},\n" +
-                "{\n" +
-                "\"id\":7,\n" +
-                "\"link\":\"\",\n" +
-                "\"name\":\"代码混淆 安全\",\n" +
-                "\"order\":7,\n" +
-                "\"visible\":1\n" +
-                "},\n" +
-                "{\n" +
-                "\"id\":8,\n" +
-                "\"link\":\"\",\n" +
-                "\"name\":\"逆向 加固\",\n" +
-                "\"order\":8,\n" +
-                "\"visible\":1\n" +
-                "}\n" +
-                "],\n" +
-                "\"errorCode\":0,\n" +
-                "\"errorMsg\":\"\"\n" +
-                "}";
-
-        JsonData jsonData = decodeJson(jsonString); // 解析 JSON 字符串
-        setData(jsonData);
+        sendGetRequest("https://www.wanandroid.com/hotkey/json",jsonData -> {setText(jsonData);});
+//        mHandler=new MyHandler();
+//        if (jsonData != null) setText(jsonData);
+        Log.d("onCreate","test");
     }
 
     private void initView() {
         mTvJson = findViewById(R.id.JSON_text);
     }
 
-    private void setData(JsonData jsonData) {
+    private void setText(JsonData jsonData){
         mTvJson.setText(jsonData.toString());
+    }
+
+    private void sendGetRequest(String mUrl, JsonDataListener listener){
+        //lambda表达式，相当于其中new Runnable并且重写方法
+        new Thread(
+                () -> {
+                    try {
+                        HttpURLConnection connection = getHttpURLConnection(mUrl);
+                        connection.connect();//正式连接
+                        InputStream in = connection.getInputStream();//从接口处获取
+                        String responseData = StreamToString(in);//这里就是服务器返回的数据
+                        Log.d("lx", "sendGetNetRequest: "+responseData);
+                        jsonData = decodeJson(responseData); // 解析 JSON 字符串
+                        if (jsonData != null){
+                            runOnUiThread(() -> {
+                                listener.onDataReceived(jsonData);
+                            });
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+        ).start();
+    }
+
+    @NonNull
+    private static HttpURLConnection getHttpURLConnection(String mUrl) throws IOException {
+        URL url = new URL(mUrl);
+        HttpURLConnection connection = (HttpURLConnection)
+                url.openConnection();
+        connection.setRequestMethod("GET");//设置请求方式为GET
+        connection.setConnectTimeout(8000);//设置最大连接时间，单位为ms
+        connection.setReadTimeout(8000);//设置最大的读取时间，单位为ms
+        connection.setRequestProperty("Accept-Language",
+                "zh-CN,zh;q=0.9");
+        connection.setRequestProperty("Accept-Encoding",
+                "gzip,deflate");
+        return connection;
+    }
+
+    private class MyHandler extends Handler {
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
+            String result = (String) msg.obj;
+            setText(decodeJson(result));
+        }
+    }
+
+    private String StreamToString(InputStream in) {
+        StringBuilder sb = new StringBuilder();
+        String oneLine;
+        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+        try {
+            while ((oneLine = reader.readLine()) != null) {
+                sb.append(oneLine).append('\n');
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                in.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return sb.toString();
     }
 
     private JsonData decodeJson(String data) {
